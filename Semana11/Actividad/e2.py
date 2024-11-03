357a7a3a8624a0cd43e8d4ddb5883d5e19194a53
JayGhz <jaykansas7@gmail.com>
2024-11-03 16:11:36 -0500

    Add graph data and implement Ford Fulkerson algorithm in e2.py

---
+++ Semana11/Actividad/e2.py
@@ -0,0 +1,46 @@
+import math
+
+def findAugmentingPath(G, s, t):
+    n = len(G)
+    visited = [False]*n
+    augPath = []
+
+    def dfs(u, bottleNeck):
+        visited[u] = True
+        augPath.append(u)
+        if u == t:
+            return bottleNeck
+        for v in range(n):
+            if G[u, v] > 0 and not visited[v]:
+                bn2 = dfs(v, bottleNeck if bottleNeck <= G[u, v] else G[u, v])
+                if visited[t]:
+                    return bn2
+        augPath.pop()
+
+    bottleNeck = dfs(s, math.inf)
+
+    return augPath, bottleNeck
+
+
+def fordFulkerson(G, s, t):
+    n = len(G)
+    Gres = G.copy()
+    for i in range(n):
+        for j in range(n):
+            if not np.isnan(Gres[i, j]) and np.isnan(Gres[j, i]):
+                Gres[j, i] = 0
+    Gflow = np.zeros((n, n))
+
+    maxFlow = 0
+    augmentingPath, bottleNeck = findAugmentingPath(Gres, s, t)
+    while augmentingPath:
+        for i in range(len(augmentingPath) - 1):
+            u = augmentingPath[i]
+            v = augmentingPath[i+1]
+            Gres[u, v] -= bottleNeck
+            Gres[v, u] += bottleNeck
+            Gflow[u, v] += bottleNeck
+        maxFlow += bottleNeck
+        augmentingPath, bottleNeck = findAugmentingPath(Gres, s, t)
+
+    return maxFlow, Gflow
